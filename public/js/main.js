f760092bc8c0612d984cf3d521296e072f6e5340
emily <limeyb7@gmail.com>
2016-11-10 12:40:27 +0000

    write buildUrl function with tests

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,15 @@ function good(){
   return true;
 }
 
+function buildUrl(endpoint, lang, value){
+  if (arguments.length != 3) return undefined;
+  return endpoint + "?lang=" + lang + "&search=" + value;
+}
+
 
 if (module) {
 module.exports = {
-  good: good
+  good: good,
+  buildUrl: buildUrl
 }
 }
--- tests/front-end-tests.js
+++ tests/front-end-tests.js
@@ -2,7 +2,15 @@ var test = require('tape');
 var main = require("../public/js/main.js");
 
 
-test('testing our server', function (t) {
+test('test that tests are working!', function (t) {
   t.ok(main.good, "detects good func");
   t.end();
 });
+
+test('test that buildUrl returns a url formatted string', function (t) {
+  t.ok(typeof main.buildUrl("/dict", "en", "a") == "string", "should return a string");
+  t.ok(main.buildUrl("/dict", "en", "a") == "/dict?lang=en&search=a", "should return properly formatted url");
+  t.ok(main.buildUrl("/dict", "en") === undefined, "when third parameter missing, should return undefined");
+  t.ok(main.buildUrl("/dict") === undefined, "when second and third parameter missing, should return undefined");
+  t.end();
+})
