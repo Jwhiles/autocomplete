6a17b4cca5299dca8b1fa73edb4fd05602ee5e18
John Whiles <johnwhiles@outlook.com>
2016-11-10 13:02:42 +0000

    add a function that returns array of words amtching search terms

--- tests/test1.js
+++ tests/test1.js
@@ -2,12 +2,22 @@ const test = require('tape');
 const search = require('../src/search.js');
 
 test('test loading dictionary', function (t) {
-  search.import(function(err, data) {
+  search.import(function (err, data) {
     if (err) {
       console.log(err);
       return;
     }
     t.ok(data);
+    t.end();
+  });
+});
+test('test dictionary is expected length', function (t) {
+  search.import(function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    t.equal(data.length, 235887);
     t.end();
   });
 });
