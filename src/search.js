e187a7135e9827486c9b29123c0dbe8fd6c44a87
John Whiles <johnwhiles@outlook.com>
2016-11-14 15:26:38 +0000

    improve language specific greeting, enlarge title

--- src/search.js
+++ src/search.js
@@ -3,7 +3,6 @@ var path = require('path');
 var search = module.exports = {};
 
 var words = [];
-var currentLanguage;
 var languages = {
   'en': 'english.txt',
   'fr': 'French.dic',
@@ -12,9 +11,10 @@ var languages = {
   'hu': 'Hungarian.dic',
   'it': 'Italian.dic',
   'du': 'Dutch.dic',
-  'no': 'Norwegian (Bokmal).dic',
+  'no': 'Norwegian.dic',
   'sp': 'Spanish.dic',
   'da': 'Danish.dic'
+
 };
 
 search.import = function (language, cb) {
@@ -39,44 +39,30 @@ search.find = function (searchTerm, language, cb) {
   if (arguments.length !== 3) {
     return ('Not Enough Arguments');
   }
-  var data = words;
-  if (language !== currentLanguage) {
-    search.import(language, function (err, newData) {
-      if (err) {
-        cb('Problem Importing');
+  search.import(language, function (err, data) {
+    if (err) {
+      cb(err);
+    }
+    var pattern = new RegExp('^' + searchTerm, 'i');
+    var results = [];
+    // var firstIndex = false;
+
+    for (var i = 0; i < data.length; i++) {
+      // var currentMatch = false;
+      if (pattern.test(data[i])) {
+        // if (!firstIndex) {
+        //   firstIndex = i;
+        // }
+        results.push(data[i]);
+        // currentMatch = true;
       }
-      data = newData;
-      currentLanguage = language;
-      search.filterBy(searchTerm, data, cb);
-    });
-  } else {
-    search.filterBy(searchTerm, data, cb);
-  }
-};
-
-search.filterBy = function (searchTerm, data, cb) {
-  var pattern = new RegExp('^' + searchTerm, 'i');
-  var results = [];
-
-  for (var i = 0; i < data.length; i++) {
-    if (pattern.test(data[i])) {
-      results.push(data[i]);
+      if (results.length >= 50) {
+        break;
+      }
     }
-    if (results.length >= 50) {
-      break;
-    }
-  }
-  var responseObj = {};
-  responseObj.results = results;
-  responseObj.matchCount = results.length;
-  cb(null, JSON.stringify(responseObj));
+    var responseObj = {};
+    responseObj.results = results;
+    responseObj.matchCount = results.length;
+    cb(null, JSON.stringify(responseObj));
+  });
 };
-
-// Init
-search.import('en', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  currentLanguage = 'en';
-  words = data;
-});
